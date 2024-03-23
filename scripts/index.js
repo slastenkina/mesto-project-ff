46b392250b4807bd083a9b71db12d069e091b1c7
Slastenkina <mia.slasta@gmail.com>
2024-03-24 02:52:57 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardInfo, callbackDelete) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate
+    .querySelector('.places__item')
+    .cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = cardInfo.link;
+  cardElement.querySelector('.card__image').alt = cardInfo.name;
+  cardElement.querySelector('.card__title').textContent = cardInfo.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', callbackDelete);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const removeCard = event.target.closest('.card');
+  removeCard.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardInfo) => {
+  const card = addCard(cardInfo, deleteCard);
+  cardsContainer.append(card);
+});
